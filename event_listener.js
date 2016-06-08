8dd38e20d2a107ea113ffeaadc2a573475d81164
Shaoshuai Dong <dongshaoshuai@ucloud.cn>
2016-06-08 17:48:23 +0800

    Use pm2-events

--- event_listener.js
+++ event_listener.js
@@ -1,22 +1,16 @@
-var axon  = require('pm2-axon');                                      
-var sub = axon.socket('sub-emitter');
-var exec = require('child_process').exec;
+var pmEvents = require('pm2-events');
+
+var myPm = new pmEvents();
 
 var listener = function(io) {
-    exec('ls ~/.pm2/pub.sock', function(error, stdout, stderr) {
-        if (error || stderr) {
-            GLOBAL.logger.error('"ls ~/.pm2/pub.sock failed" ', error, stderr);
-        } else {
-            var sock = stdout;
-            // default ~/.pm2/pub.sock
-            sub.connect(sock);
-            // Events : restart, online, exit, restart overlimit, ***
-            sub.on('process:*', function(e, p) {
-                io.emit('event', JSON.stringify({
-                    event: p
-                }));
-            });
-        }
+    myPm
+    .on(function(e, p) {
+        io.emit('event', JSON.stringify({
+            event: p
+        }));
+    })
+    .error(function(err) {
+        GLOBAL.logger.error('pm2-events error:', err);
     });
 };
 
